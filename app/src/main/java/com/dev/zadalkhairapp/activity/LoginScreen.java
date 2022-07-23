520831618625a13b442e001b3f7623423ef44350
Rozan.I.Madi <74018018+RozanMadi95@users.noreply.github.com>
2022-07-23 20:57:47 +0300

    LoginScreen

--- app/src/main/java/com/dev/zadalkhairapp/activity/LoginScreen.java
+++ app/src/main/java/com/dev/zadalkhairapp/activity/LoginScreen.java
@@ -2,15 +2,52 @@ package com.dev.zadalkhairapp.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageButton;
+import androidx.appcompat.widget.AppCompatTextView;
+
+import com.dev.zadalkhairapp.MainActivity;
 import com.dev.zadalkhairapp.R;
+import com.google.android.material.textfield.TextInputEditText;
+
 
 public class LoginScreen extends AppCompatActivity {
-
+    TextInputEditText etPhone, etPassword;
+    AppCompatTextView  tvForgotPassword, tvSingUp;
+    AppCompatButton btnLogin;
+    AppCompatImageButton butImgLoginPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        findViewById();
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginScreen.this, MainActivity.class));
+                finish();
+            }
+        });
+
+        tvSingUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginScreen.this,SignupScreen.class));
+                finish();
+            }
+        });
+    }
+    public void findViewById() {
+        etPhone = findViewById(R.id.loginEtPhone);
+        etPassword = findViewById(R.id.loginEtPassword);
+        tvForgotPassword = findViewById(R.id.loginTVForgotPassword);
+        tvSingUp = findViewById(R.id.loginTVSingUp);
+        btnLogin = findViewById(R.id.loginButton);
+        butImgLoginPhone = findViewById(R.id.loginButtonPhone);
     }
 }
---
+++ app/src/main/java/com/dev/zadalkhairapp/activity/SignupScreen.java
@@ -0,0 +1,16 @@
+package com.dev.zadalkhairapp.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dev.zadalkhairapp.R;
+
+public class SignupScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup_screen);
+    }
+}
